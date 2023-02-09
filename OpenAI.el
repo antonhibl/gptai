5ebe81799730a2ca07261b7e83dcaa4ab1044284
Anton Hibl <antonhibl11@gmail.com>
2023-02-08 21:05:59 -0700

    bruh

--- OpenAI.el
+++ OpenAI.el
@@ -6,7 +6,6 @@
 ;; URL: https://github.com/antonhibl/openai
 ;; Keywords: AI
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "28.2"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
