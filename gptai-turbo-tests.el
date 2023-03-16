7a59ef2bea553c4e8a4fb2982678d2ccd95fc57b
Anton Hibl <antonhibl11@gmail.com>
2023-03-16 00:17:55 -0600

    added more modular functionality to the package
        added more turbo API functionality and a suite of tests for this section of
        the package. To run the tests, you can either use M-x ert or evaluate the
        following code in the *scratch* buffer or another buffer:
    
        (ert-run-tests-batch-and-exit)
    
        M-x ert will open the ERT interactive test runner, where you can select the
        tests you want to run. Press t to run all the tests, or use r to run a
        specific test by its name. The test results will be displayed in the ERT
        test runner buffer.
    
        By running (ert-run-tests-batch-and-exit), the tests will be executed in batch
        mode, and the test results will be displayed in the *Messages* buffer. If
        all tests pass, you will see the message All tests passed.
    
        These tests use a mock API response and a test function that simulates
        gptai-turbo-request without making an actual API call to ensure that the
        tests do not depend on the availability of the API and to avoid
        rate-limiting issues. The test cases are designed to check if the
        gptai-turbo-request and gptai-turbo-response functions return and insert the
        expected content, respectively.

---
+++ gptai-turbo-tests.el
@@ -0,0 +1,84 @@
+;;; gptai-turbo-tests.el --- Test Suite for gptai-turbo.el -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023 Hibl, Anton
+
+;; Author: Anton Hibl <antonhibl11@gmail.com>
+;; URL: https://github.com/antonhibl/gptai
+;; Keywords: comm, convenience
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24.1"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; A suite of tests for the gptai-turbo functions to ensure they work as
+;; expected. To run these tests either:
+
+;; A. M-x ert will open the ERT interactive test runner, where you can select
+;; the tests you want to run. Press t to run all the tests, or use r to run a
+;; specific test by its name. The test results will be displayed in the ERT test
+;; runner buffer. 
+
+;; B. By running (ert-run-tests-batch-and-exit), the tests will be executed in
+;; batch mode, and the test results will be displayed in the *Messages*
+;; buffer. If all tests pass, you will see the message All tests passed.
+
+;; These tests use a mock API response and a test function that simulates
+;; gptai-turbo-request without making an actual API call to ensure that the
+;; tests do not depend on the availability of the API and to avoid rate-limiting
+;; issues. 
+
+;;; Code:
+
+(require 'ert)
+(require 'gptai-turbo)
+
+;; Mock API response
+(setq mock-api-response
+      '((id . "chatcmpl-6uaMfmuqdi2R0Zj5HSgKvcUjQceeJ")
+        (object . "chat.completion")
+        (created . 1678944173)
+        (model . "gpt-3.5-turbo-0301")
+        (usage (prompt_tokens . 11) (completion_tokens . 41) (total_tokens . 52))
+        (choices . [((message (role . "assistant") (content . "This is a test response.")) (finish_reason . "stop") (index . 0))])))
+
+;; Test function that simulates gptai-turbo-request without making an actual API call
+(defun gptai-turbo-request-test (gptai-prompt)
+  "Simulates gptai-turbo-request without making an actual API call."
+  (let ((first-choice (elt (cdr (assoc 'choices mock-api-response)) 0)))
+    (cdr (assoc 'content (cdr (assoc 'message first-choice))))))
+
+(ert-deftest gptai-turbo-request-test ()
+  "Test gptai-turbo-request with a mock response."
+  (let* ((prompt "Test prompt")
+         (gptai-turbo-request (symbol-function 'gptai-turbo-request)))
+    (fset 'gptai-turbo-request (symbol-function 'gptai-turbo-request-test))
+    (should (string= (gptai-turbo-request prompt) "This is a test response."))
+    (fset 'gptai-turbo-request gptai-turbo-request)))
+
+(ert-deftest gptai-turbo-response-test ()
+  "Test gptai-turbo-response with a mock response."
+  (let* ((prompt "Test prompt")
+         (gptai-turbo-request (symbol-function 'gptai-turbo-request))
+         (buffer (generate-new-buffer "*gptai-turbo-response-test*")))
+    (fset 'gptai-turbo-request (symbol-function 'gptai-turbo-request-test))
+    (with-current-buffer buffer
+      (gptai-turbo-response prompt)
+      (should (string= (buffer-string) "This is a test response.")))
+    (fset 'gptai-turbo-request gptai-turbo-request)
+    (kill-buffer buffer)))
+
+(provide 'gptai-turbo-tests)
+;;; gptai-turbo-tests.el ends here
--- gptai-turbo.el
+++ gptai-turbo.el
@@ -1,3 +1,35 @@
+;;; gptai-turbo.el --- Integrate with the gpt-3.5-turbo model -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023 Hibl, Anton
+
+;; Author: Anton Hibl <antonhibl11@gmail.com>
+;; URL: https://github.com/antonhibl/gptai
+;; Keywords: comm, convenience
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24.1"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; A rough start to integrating the newer chat endpoints into emacs, working
+;; towards a larger chat functionality for this package and the editor as a
+;; whole.
+
+;;; Code:
+
+
 (defun gptai-turbo-request (gptai-prompt)
   "Sends a request to OpenAI API's gpt-3.5-turbo endpoint and returns the response.
 Argument GPTAI-PROMPT is the prompt to send to the API."
@@ -38,3 +70,6 @@ Argument GPTAI-PROMPT prompt to be sent."
   (interactive "sEnter your prompt: ")
   (let ((response (gptai-turbo-request gptai-prompt)))
     (insert response)))
+
+(provide 'gptai-turbo)
+;;; gptai-turbo.el ends here
